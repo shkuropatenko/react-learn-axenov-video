e98b581b8f66c7b73390929c8c8dac4069d506e3
DmitriyShkuropatenko <dmitriy.sh@darly.solutions>
2022-05-23 22:47:26 -0700

    added event onClick paragraph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import Header from "./Header";
 import { useState } from "react";
 
-const Form = (props) => {
-  if(!props.isShow) {
+const users = [
+  {id: 1, name: 'Petya'},
+  {id: 2, name: 'Vasya'},
+  {id: 3, name: 'Stepa'}
+]
+
+const Form = ({isShow, clickName}) => {
+  if(!isShow) {
     return null;
   }
-  return <h3>Render my Form</h3>
+  return users.map(({id, name}) => <p onClick={clickName} key={id}>{name}</p>)
 }
 
 function App() {
   const [isShow, setIsShow] = useState(false);
+
   const changeIsShow = () => {
     setIsShow(!isShow)
+  }
+
+  const clickName = (event) => {
+    console.log(event.target)
+    event.target.style='background: red'
   }
 
   return (
     <div className="App">
       <h1>Hello App</h1>
       <button onClick={changeIsShow}>Show Form</button>
-      <Form isShow={isShow} />
+      <Form isShow={isShow} clickName={clickName } />
     </div>
   );
 }
