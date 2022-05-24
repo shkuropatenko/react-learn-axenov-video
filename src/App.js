84a81970eeb905f7d51430c7f60564c9b13527fb
DmitriyShkuropatenko <dmitriy.sh@darly.solutions>
2022-05-23 22:30:09 -0700

    added state Boolean to show/hide Form

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import Header from "./Header";
 import { useState } from "react";
-import fields from './fields.json';
 
-const List = () => fields.map((field) => <input key={field.id} type={field.type} name={field.name} placeholder={field.placeholder} />)
+const Form = (props) => {
+  if(!props.isShow) {
+    return null;
+  }
+  return <h3>Render my Form</h3>
+}
 
 function App() {
-  const [user, setUser] = useState({
-    name: 'Dima',
-    age: 32,
-    email: 'mail@gmail.com',
-  });
-
-  const onChangeName = (event) => {
-    const value = event.target.value;
-    const key = event.target.name;
-
-    setUser({...user, [key]: value})
-    console.log(value)
+  const [isShow, setIsShow] = useState(false);
+  const changeIsShow = () => {
+    setIsShow(!isShow)
   }
-
-  console.log('RENDER APP COMPONENT')
-  const {name, age, email, lastName} = user;
 
   return (
     <div className="App">
-      <p>name: {name}</p>
-      <p>age: {age}</p>
-      <p>email: {email}</p>
-      <p>lastName: {lastName}</p>
-      <List />
+      <h1>Hello App</h1>
+      <button onClick={changeIsShow}>Show Form</button>
+      <Form isShow={isShow} />
     </div>
   );
 }
